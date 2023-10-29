9d4040d172bd8cabeac4e7740a11edc46a18d289
Faiz Khan <khanfaiz33897@gmail.com>
2023-10-29 14:16:34 +0530

    added backend host

--- Frontend/src/store/DishesSlice.js
+++ Frontend/src/store/DishesSlice.js
@@ -1,33 +1,35 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 // Action
 export const getAllData = createAsyncThunk("dishes", async () => {
-    const response = await fetch("http://localhost:3000/api/dishes/getalldishes");
-    const result = await response.json();
-    return result;
+  const response = await fetch(
+    "https://food-delivery-app-lyart.vercel.app/api/dishes/getalldishes"
+  );
+  const result = await response.json();
+  return result;
 });
 
 export const dishes = createSlice({
-    name: 'dishes',
-    initialState: {
-        dishes: [],
-        loading: false,
-        error: null,
-    },
+  name: "dishes",
+  initialState: {
+    dishes: [],
+    loading: false,
+    error: null,
+  },
 
-    extraReducers: {
-        [getAllData.pending]: (state) => {
-            state.loading = true;
-        },
-        [getAllData.fulfilled]: (state, action) => {
-            state.loading = false;
-            state.dishes = action.payload;
-        },
-        [getAllData.rejected]: (state, action) => {
-            state.loading = false;
-            state.error = action.payload;
-        },
-    }
+  extraReducers: {
+    [getAllData.pending]: (state) => {
+      state.loading = true;
+    },
+    [getAllData.fulfilled]: (state, action) => {
+      state.loading = false;
+      state.dishes = action.payload;
+    },
+    [getAllData.rejected]: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+  },
 });
 
 export default dishes.reducer;
