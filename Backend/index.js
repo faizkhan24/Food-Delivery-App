ecc89141ff1f3427858983eaf6d066b8751ff245
Faiz Khan <khanfaiz33897@gmail.com>
2023-10-29 15:01:56 +0530

    bug fixes

--- Backend/index.js
+++ Backend/index.js
@@ -3,12 +3,15 @@ const Dish = require("./Models/dishesModel");
 const cors = require("cors");
 
 const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
 const db = require("./db");
 app.use(express.json());
 const dishesRoute = require("./routes/dishesRoutes");
 const userRoute = require("./routes/userRoute");
 
-app.use(cors());
 app.use("/api/dishes", dishesRoute);
 app.use("/api/users", userRoute);
 app.use("/api/users", userRoute);
@@ -16,8 +19,6 @@ app.use("/api/users", userRoute);
 app.get("/", (req, res) => {
   res.send("Server working on port " + port);
 });
-
-const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
