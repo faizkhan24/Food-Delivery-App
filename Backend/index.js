0a1c19f0938ae81d97db432d6c8e6fa2625e2b55
Faiz Khan <khanfaiz33897@gmail.com>
2023-10-29 14:25:36 +0530

    bug fixes

--- Backend/index.js
+++ Backend/index.js
@@ -1,28 +1,24 @@
-const express = require('express');
-const Dish = require('./Models/dishesModel');
-
-
+const express = require("express");
+const Dish = require("./Models/dishesModel");
+const cors = require("cors");
 
 const app = express();
-const db = require('./db');
+const db = require("./db");
 app.use(express.json());
-const dishesRoute = require('./routes/dishesRoutes')
-const userRoute = require('./routes/userRoute')
+const dishesRoute = require("./routes/dishesRoutes");
+const userRoute = require("./routes/userRoute");
 
+app.use(cors());
+app.use("/api/dishes", dishesRoute);
+app.use("/api/users", userRoute);
+app.use("/api/users", userRoute);
 
-
-app.use('/api/dishes',dishesRoute)
-app.use('/api/users',userRoute)
-app.use('/api/users',userRoute)
-
-app.get('/', (req, res) => {
-    res.send('Server working on port ' + port);
+app.get("/", (req, res) => {
+  res.send("Server working on port " + port);
 });
-
-
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
