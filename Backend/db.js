5a9ea242628abf6ec4e6ced2665a5d5ef444e82e
Faiz Khan <khanfaiz33897@gmail.com>
2023-10-29 11:29:36 +0530

    bug fixes

--- Backend/db.js
+++ Backend/db.js
@@ -1,17 +1,18 @@
 const mongoose = require("mongoose");
 
-var mongoURL = 'mongodb://0.0.0.0:27017/mern-fooddelivery-app';
+var mongoURL =
+  "mongodb+srv://faizkhan:TjxoQeK0YPKoasUA@cluster0.binjoim.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose.connect(mongoURL, { useUnifiedTopology: true, useNewUrlParser: true }); // Fixed 'usenewUrlParser' to 'useNewUrlParser'
 
 var db = mongoose.connection;
 
-db.on('connected', () => {
-    console.log('mongo DB connection successful');
+db.on("connected", () => {
+  console.log("mongo DB connection successful");
 });
 
-db.on('error', () => {
-    console.log('mongodb connection failed');
+db.on("error", () => {
+  console.log("mongodb connection failed");
 });
 
 module.exports = mongoose;
