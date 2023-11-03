64e24b2e7e454d83a7d5fee801981b29d08f664f
Faiz Khan <khanfaiz33897@gmail.com>
2023-11-03 21:53:35 +0530

    adding payment gateway

--- Frontend/src/components/Checkout.js
+++ Frontend/src/components/Checkout.js
@@ -1,13 +1,36 @@
-import React from 'react'
-import StripeCheckout from 'react-stripe-checkout'
-const Checkout = () => {
+// Checkout.js
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import {useDispatch, useSelector} from 'react-redux'
+
+const Checkout = ({ calculateSubtotal }) => {
+  const dispatch = useDispatch();
+
+  function tokenHandler(token) {
+    console.log('Token:', token);
+    const subtotal = calculateSubtotal();
+    console.log('Subtotal:', subtotal);
+    const amount = subtotal;
+    console.log('Amount:', amount);
+
+
+  }
+
+  // ...rest of the code
+
+
+
+  // Calculate the amount using the calculateSubtotal function
+  const amount = calculateSubtotal()*100;
+
   return (
     <div>
-      <StripeCheckout>
-      <button className="paynow-btn">Pay now</button>
+      <StripeCheckout amount={amount} shippingAddress token={tokenHandler} currency="INR" stripeKey={'pk_test_51O7KJeSIFQNRHGYfjvzwMiR5KEK0YAf6tbF5K5QjERfcDLYE2RxGDTxCwPQ375HLIiWHYuMZydZEZxkFmzLOYq4N00YxwP303r'}>
+        <button className="paynow-btn">Pay now</button>
       </StripeCheckout>
     </div>
-  )
-}
+  );
+}; 
 
-export default Checkout
+
+export default Checkout;
