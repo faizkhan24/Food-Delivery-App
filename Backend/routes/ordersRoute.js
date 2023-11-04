6225e8dcf69af68b022eff162959e978d156b593
Faiz Khan <khanfaiz33897@gmail.com>
2023-11-04 12:37:54 +0530

    adding order route

--- Backend/routes/ordersRoute.js
+++ Backend/routes/ordersRoute.js
@@ -1,11 +1,41 @@
-const express = require("express")
+const express = require("express");
 const router = express.Router();
-const stripe = require("stripe")("sk_test_51O7KJeSIFQNRHGYfPTGkU9AolWSjnyoTfBxGn1MWiAjmMUVmBHneJPLuh8wclzTF89LGGIlounPanuKACmZvc17C00xlUXqf3D")
+const { v4: uuidv4 } = require("uuid");
+const stripe = require("stripe")(
+  "sk_test_51O7KJeSIFQNRHGYfPTGkU9AolWSjnyoTfBxGn1MWiAjmMUVmBHneJPLuh8wclzTF89LGGIlounPanuKACmZvc17C00xlUXqf3D"
+);
 
-router.post("/placeorder",(req , res)=>{
+router.post("/placeorder", async (req, res) => {
+  const { token, calculateSubtotal, currentUser, cartState } = req.body;
 
-    const {token,calculateSubtotal, currentUser,  cartState} = req.body
-    
+  try {
+    const customer = await stripe.customers.create({
+      email: token.email,
+      source: token.id,
+    });
+
+    const payment = await stripe.paymentIntents.create(
+      {
+        amount: calculateSubtotal * 100,
+        currency: "inr",
+        customer: customer.id,
+        receipt_email: token.email,
+      },
+      {
+        idempotencyKey: uuidv4(),
+      }
+    );
+    console.log("payment", payment);
+
+    if (payment) {
+      res.send("Payment Done");
+    } else {
+      res.send("Payment failed");
+    }
+  } catch (error) {
+    return res.status(400).json({ message: "Bad Request", error: error.message });
+      
+  }
 });
 
 module.exports = router;
